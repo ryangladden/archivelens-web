3b5105291542cdf0f36fd46d7e5dcf0676a362a0
Ryan Gladden <ryangladden@gertrude>
2025-04-18 06:12:22 -0500

    Initial commit (svelteKit)

---
+++ src/lib/header/header.svelte
@@ -0,0 +1,4 @@
+<div class="inline-flex rounded-sm items-center bg-black w-full text-white">
+    <img src="archive-lens.svg" alt="Archive Lens" class="h-16" />
+    <p class="font-mono text-xl">rchive Lens</p>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	import Sidebar from '$lib/sidebar/sidebar.svelte';
+	import Header from '$lib/header/header.svelte';
+	let { children } = $props();
+</script>
+<Header />
+<div class="flex-inline">
+<Sidebar />
+{@render children()}
+</div>
